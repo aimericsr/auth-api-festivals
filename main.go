2c5a0000f19914f9d51538f7b2ec0241572ac523
aimericsr <52284219+aimericsr@users.noreply.github.com>
2022-04-20 11:33:49 +0200

    test

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/aimericsr/auth-api-festivals/db"
 	"github.com/aimericsr/auth-api-festivals/util"
 )
 
 func main() {
-	config, err := util.LoadConfig(".")
+	_, err := util.LoadConfig(".")
 	if err != nil {
 		log.Fatal("Cannot load config variables:", err)
 	}
-
-	db.Init(config.DBSource)
-
 }
